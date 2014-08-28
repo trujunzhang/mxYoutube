2c48b55ffcf149b6486c5a7ef66d7d6d58e93230
wanghaogithub720 <wanghaomspace@gmail.com>
2014-08-28 10:35:08 +0800

    develop home fragment.

---
+++ android/mxYoutubeApp/app/src/main/java/com/mxtube/app/ui/single/Home.java
@@ -0,0 +1,11 @@
+package com.mxtube.app.ui.single;
+
+
+import com.mxtube.app.R;
+import org.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.single_home)
+public class Home extends Single{
+
+
+}
