fd2a172098c570a089a7b52ef0f30a9d2193fd1b
wanghaogithub720 <wanghaomspace@gmail.com>
2014-08-28 13:10:09 +0800

    develop home fragment.

--- android/mxYoutubeApp/app/src/main/java/com/mxtube/app/ui/single/Home.java
+++ android/mxYoutubeApp/app/src/main/java/com/mxtube/app/ui/single/Home.java
@@ -1,11 +1,13 @@
 package com.mxtube.app.ui.single;
 
-
 import com.mxtube.app.R;
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
 
 @EFragment(R.layout.single_home)
-public class Home extends Single{
+public class Home extends Single {
 
+	@ViewById(R.id.gridView)
+	android.widget.GridView gridView;
 
 }
