dc3f5d71c0f7aa1783cf3fe2bfa813aa6600cbcb
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-01 17:29:50 +0800

    developing app.

--- android/mxYoutubeApp/libraries/open/android-youtube-player/src/main/java/com/keyes/youtube/ui/Sample.java
+++ android/mxYoutubeApp/libraries/open/android-youtube-player/src/main/java/com/keyes/youtube/ui/Sample.java
@@ -7,35 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.keyes.youtube.R;
+import com.google.android.libraries.mediaframework.R;
 
 public class Sample extends Activity {
 
     @Override
     protected void onCreate(Bundle pSavedInstanceState) {
         super.onCreate(pSavedInstanceState);
-
-        setContentView(R.layout.sample);
-
-        final TextView videoIdTextView = (TextView) findViewById(R.id.youtubeIdText);
-        final Button viewVideoButton = (Button) findViewById(R.id.viewVideoButton);
-
-        viewVideoButton.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View pV) {
-
-                String videoId = videoIdTextView.getText().toString();
-
-                if (videoId == null || videoId.trim().equals("")) {
-                    return;
-                }
-
-                Uri parse = Uri.parse("ytv://" + videoId);
-                Intent lVideoIntent = new Intent(null, parse, Sample.this, OpenYouTubePlayerActivity.class);
-                startActivity(lVideoIntent);
-
-            }
-        });
+//
+//        setContentView(R.layout.sample);
+//
+//        final TextView videoIdTextView = (TextView) findViewById(R.id.youtubeIdText);
+//        final Button viewVideoButton = (Button) findViewById(R.id.viewVideoButton);
+//
+//        viewVideoButton.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View pV) {
+//
+//                String videoId = videoIdTextView.getText().toString();
+//
+//                if (videoId == null || videoId.trim().equals("")) {
+//                    return;
+//                }
+//
+//                Uri parse = Uri.parse("ytv://" + videoId);
+//                Intent lVideoIntent = new Intent(null, parse, Sample.this, OpenYouTubePlayerActivity.class);
+//                startActivity(lVideoIntent);
+//
+//            }
+//        });
 
     }
 
