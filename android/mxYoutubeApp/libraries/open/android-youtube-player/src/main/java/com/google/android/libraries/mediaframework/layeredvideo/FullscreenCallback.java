91da6f1970c9e233382390acdccb665b50be5b15
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-01 17:53:14 +0800

    developing app.

---
+++ android/mxYoutubeApp/libraries/open/android-youtube-player/src/main/java/com/google/android/libraries/mediaframework/layeredvideo/FullscreenCallback.java
@@ -0,0 +1,20 @@
+package com.google.android.libraries.mediaframework.layeredvideo;
+
+/**
+ * In order to imbue the {@link com.google.android.libraries.mediaframework.layeredvideo.PlaybackControlLayer} with the ability make the player fullscreen, a
+ * {@link FullscreenCallback} must be assigned to it. The
+ * {@link FullscreenCallback} implementation is responsible for hiding/showing the other views
+ * on the screen when the player enters/leaves fullscreen mode.
+ */
+public interface FullscreenCallback {
+
+    /**
+     * When triggered, the activity should hide any additional views.
+     */
+    public void onGoToFullscreen();
+
+    /**
+     * When triggered, the activity should show any views that were hidden when the player went to fullscreen.
+     */
+    public void onReturnFromFullscreen();
+}
--- android/mxYoutubeApp/libraries/open/android-youtube-player/src/main/java/com/google/android/libraries/mediaframework/layeredvideo/PlaybackControlLayer.java
+++ android/mxYoutubeApp/libraries/open/android-youtube-player/src/main/java/com/google/android/libraries/mediaframework/layeredvideo/PlaybackControlLayer.java
@@ -90,26 +90,7 @@ import java.util.Locale;
  */
 public class PlaybackControlLayer implements Layer, PlayerControlCallback {
 
-	/**
-	 * In order to imbue the {@link PlaybackControlLayer} with the ability make the player fullscreen, a
-	 * {@link PlaybackControlLayer.FullscreenCallback} must be assigned to it. The
-	 * {@link PlaybackControlLayer.FullscreenCallback} implementation is responsible for hiding/showing the other views
-	 * on the screen when the player enters/leaves fullscreen mode.
-	 */
-	public interface FullscreenCallback {
-
-		/**
-		 * When triggered, the activity should hide any additional views.
-		 */
-		public void onGoToFullscreen();
-
-		/**
-		 * When triggered, the activity should show any views that were hidden when the player went to fullscreen.
-		 */
-		public void onReturnFromFullscreen();
-	}
-
-	/**
+    /**
 	 * Message handler which allows us to send delayed messages to the {@link PlaybackControlLayer} This is useful for
 	 * fading out the view after a certain time.
 	 */
