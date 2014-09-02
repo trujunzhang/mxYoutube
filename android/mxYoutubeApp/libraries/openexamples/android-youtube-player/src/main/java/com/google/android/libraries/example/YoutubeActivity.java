cd5fc6d4684117b67b44e267c7957d7118384d6a
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-02 09:25:03 +0800

    set [webview] as playercontroller in the PlaybackControlLayer.

--- android/mxYoutubeApp/libraries/openexamples/android-youtube-player/src/main/java/com/google/android/libraries/example/YoutubeActivity.java
+++ android/mxYoutubeApp/libraries/openexamples/android-youtube-player/src/main/java/com/google/android/libraries/example/YoutubeActivity.java
@@ -2,7 +2,6 @@ package com.google.android.libraries.example;
 
 import android.app.Activity;
 import android.net.Uri;
-import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
@@ -10,10 +9,19 @@ import android.widget.LinearLayout;
 import com.google.android.libraries.mediaframework.layeredvideo.SimpleVideoPlayer;
 import com.google.android.libraries.mediaframework.layeredvideo.callback.FullscreenCallback;
 import com.google.android.libraries.mediaframework.layeredvideo.utils.Util;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
+@EActivity(R.layout.activity_youtube)
 public class YoutubeActivity extends Activity implements FullscreenCallback {
 
-	private FrameLayout videoPlayerContainer;
+	@ViewById(R.id.video_frame)
+	FrameLayout videoPlayerContainer;
+	@ViewById(R.id.button)
+	android.widget.Button button;
+	@ViewById(R.id.info_linear_layout)
+	LinearLayout info_linear_layout;
 
 	/**
 	 * This is the layout of the container before fullscreen mode has been entered. When we leave fullscreen mode, we
@@ -27,7 +35,6 @@ public class YoutubeActivity extends Activity implements FullscreenCallback {
 	 * The callback that is triggered when fullscreen mode is entered or closed.
 	 */
 	private FullscreenCallback fullscreenCallback;
-	private LinearLayout info_linear_layout;
 
 	/**
 	 * Set the callback which will be called when the player enters and leaves fullscreen mode.
@@ -55,14 +62,8 @@ public class YoutubeActivity extends Activity implements FullscreenCallback {
 		videoPlayer.setFullscreenCallback(fullscreenCallback);
 	}
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.activity_youtube);
-
-		videoPlayerContainer = (FrameLayout) this.findViewById(R.id.video_frame);
-		info_linear_layout = (LinearLayout) this.findViewById(R.id.info_linear_layout);
+	@AfterViews
+	protected void calledAfterViewInjection() {
 
 		Uri lVideoIdUri = Uri.parse("ytv://" + "AV2OkzIGykA");
 
