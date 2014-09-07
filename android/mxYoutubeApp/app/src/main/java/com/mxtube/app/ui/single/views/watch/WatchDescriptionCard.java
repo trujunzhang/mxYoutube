3679a80b0ddeb170ff346f82385ab39fe5d66a2b
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-07 09:34:41 +0800

    developing app.

--- android/mxYoutubeApp/app/src/main/java/com/mxtube/app/ui/single/views/watch/WatchDescriptionCard.java
+++ android/mxYoutubeApp/app/src/main/java/com/mxtube/app/ui/single/views/watch/WatchDescriptionCard.java
@@ -3,11 +3,14 @@ package com.mxtube.app.ui.single.views.watch;
 import android.content.Context;
 import android.widget.LinearLayout;
 
+import com.common.utils.StringUtils;
 import com.google.api.services.youtube.model.Video;
 import com.mxtube.app.R;
 
 import org.androidannotations.annotations.EViewGroup;
 import org.androidannotations.annotations.ViewById;
+
+import java.math.BigInteger;
 
 @EViewGroup(R.layout.watch_description_card)
 public class WatchDescriptionCard extends LinearLayout {
@@ -18,8 +21,8 @@ public class WatchDescriptionCard extends LinearLayout {
 	android.widget.ImageView separator;
 	@ViewById(R.id.like_button)
 	android.widget.TextView likeButton;
-	@ViewById(R.id.offline_label)
-	android.widget.TextView offlineLabel;
+	@ViewById(R.id.viewCount_label)
+	android.widget.TextView viewCountLabel;
 	@ViewById(R.id.description)
 	android.widget.TextView description;
 
@@ -30,15 +33,22 @@ public class WatchDescriptionCard extends LinearLayout {
 	public void bind(Context context, Video singleVideo) {
 		java.math.BigInteger _likeCount = singleVideo.getStatistics().getLikeCount();
 		java.math.BigInteger _disLikeCount = singleVideo.getStatistics().getDislikeCount();
-		String _likeAndDislike = String.format("% slikes, %s dislikes", _likeCount, _disLikeCount);
+		BigInteger _viewCount = singleVideo.getStatistics().getViewCount();
+
+		String _likeAndDislike = String.format("%s likes, %s dislikes", _likeCount, _disLikeCount);
 
 		String _title = singleVideo.getSnippet().getTitle();
 		String _description = singleVideo.getSnippet().getDescription();
 
+		// ...... line01
 		this.descriptionTitle.setText(_title);
-		this.description.setText(_description);
 
+		// ...... line02
 		this.likeButton.setText(_likeAndDislike);
+		this.viewCountLabel.setText(String.valueOf(_viewCount));
+
+		// ...... line03
+		this.description.setText(_description);
 	}
 
 }
