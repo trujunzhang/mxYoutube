66a495e50683d8f993fa8d257af2f7b4259316de
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-11 10:30:28 +0800

    fix list.savedInstanceState.

--- android/mxYoutubeApp/app/src/main/java/com/mxtube/app/ui/single/widget/GridViewSingle.java
+++ android/mxYoutubeApp/app/src/main/java/com/mxtube/app/ui/single/widget/GridViewSingle.java
@@ -1,4 +1,4 @@
-package com.mxtube.app.ui.single;
+package com.mxtube.app.ui.single.widget;
 
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -12,13 +12,21 @@ import com.layer.business.utils.AppConstant;
 import com.layer.business.youtube.impl.SearchImplementation;
 import com.mxtube.app.R;
 import com.mxtube.app.adapter.YoutubeListAdapter;
+import com.mxtube.app.ui.single.Single;
 
-import org.androidannotations.annotations.*;
+import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Bean;
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.UiThread;
+import org.androidannotations.annotations.ViewById;
 
 import java.util.List;
 
 @EFragment(R.layout.widget_gridview)
-public class HomeSingle extends Single {
+public class GridViewSingle extends Single {
 
 	@ViewById(R.id.gridView)
 	android.widget.GridView gridView;
