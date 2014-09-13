da328913274560eb5f3f7e9d5a0a324c443f698f
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-13 09:36:10 +0800

    developing app.

--- android/mxYoutubeApp/libraries/openexamples/actionbarsherlock/fragments/src/main/java/com/actionbarsherlock/sample/fragments/AnimationSupport/FragmentCustomAnimationSupport.java
+++ android/mxYoutubeApp/libraries/openexamples/actionbarsherlock/fragments/src/main/java/com/actionbarsherlock/sample/fragments/AnimationSupport/FragmentCustomAnimationSupport.java
@@ -14,19 +14,18 @@
  * limitations under the License.
  */
 
-package com.actionbarsherlock.sample.fragments;
+package com.actionbarsherlock.sample.fragments.AnimationSupport;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import com.actionbarsherlock.app.SherlockFragment;
+
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.sample.fragments.R;
+import com.actionbarsherlock.sample.fragments.SampleList;
 
 public class FragmentCustomAnimationSupport extends SherlockFragmentActivity {
 	int mStackLevel = 1;
@@ -75,45 +74,6 @@ public class FragmentCustomAnimationSupport extends SherlockFragmentActivity {
 		ft.replace(R.id.simple_fragment, newFragment);
 		ft.addToBackStack(null);
 		ft.commit();
-	}
-
-	public static class CountingFragment extends SherlockFragment {
-		int mNum;
-
-		/**
-		 * Create a new instance of CountingFragment, providing "num" as an argument.
-		 */
-		static CountingFragment newInstance(int num) {
-			CountingFragment f = new CountingFragment();
-
-			// Supply num input as an argument.
-			Bundle args = new Bundle();
-			args.putInt("num", num);
-			f.setArguments(args);
-
-			return f;
-		}
-
-		/**
-		 * When creating, retrieve this instance's number from its arguments.
-		 */
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			mNum = getArguments() != null ? getArguments().getInt("num") : 1;
-		}
-
-		/**
-		 * The Fragment's UI is just a simple text view showing its instance number.
-		 */
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View v = inflater.inflate(R.layout.hello_world, container, false);
-			View tv = v.findViewById(R.id.text);
-			((TextView) tv).setText("Fragment #" + mNum);
-			tv.setBackgroundDrawable(getResources().getDrawable(android.R.drawable.gallery_thumb));
-			return v;
-		}
 	}
 
 }
