6a9d997d1879783467d9258467749127b11e562c
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-16 18:33:17 +0800

    ios develop.

---
+++ ios/mxYoutubeApp/app/app/IpadGridViewCell.h
@@ -0,0 +1,13 @@
+//
+//  IpadGridViewCell.h
+//  app
+//
+//  Created by djzhang on 9/16/14.
+//  Copyright (c) 2014 xinma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IpadGridViewCell : UICollectionViewCell
+
+@end
---
+++ ios/mxYoutubeApp/app/app/IpadGridViewCell.m
@@ -0,0 +1,31 @@
+//
+//  IpadGridViewCell.m
+//  app
+//
+//  Created by djzhang on 9/16/14.
+//  Copyright (c) 2014 xinma. All rights reserved.
+//
+
+#import "IpadGridViewCell.h"
+
+@implementation IpadGridViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
