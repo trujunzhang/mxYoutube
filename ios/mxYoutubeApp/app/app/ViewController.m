ff39eb695fb1ae23c9ccf7436eaeba28daec38eb
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-21 15:21:13 +0800

    add library "collect/IOS_Collection_Code".

--- ios/mxYoutubeApp/app/app/ViewController.m
+++ ios/mxYoutubeApp/app/app/ViewController.m
@@ -125,11 +125,16 @@
 
 
 - (void)changeBackground:(BOOL)portrait {
+   UIScreen * mainScreen = [UIScreen mainScreen];
+   CGFloat scale = ([mainScreen respondsToSelector:@selector(scale)] ? mainScreen.scale : 1.0f);
+   CGFloat pixelWidth = (CGRectGetWidth(mainScreen.bounds) * scale);
+   CGFloat pixelHeight = (CGRectGetHeight(mainScreen.bounds) * scale);
+
    NSString * name = [AppResolutionHelper resolutionNameByType:[UIDevice resolution] isPortrait:portrait];
    if (portrait) {
-      self.background.frame = CGRectMake(0, 0, 768, 1024);
+      self.background.frame = CGRectMake(0, 0, pixelWidth, pixelHeight);
    } else {
-      self.background.frame = CGRectMake(0, 0, 1024, 768);
+      self.background.frame = CGRectMake(0, 0, pixelHeight, pixelWidth);
    }
 //   self.background.image = [UIImage imageNamed:@"Default-568h@2x.png"];
    self.background.image = [UIImage imageNamed:name];
