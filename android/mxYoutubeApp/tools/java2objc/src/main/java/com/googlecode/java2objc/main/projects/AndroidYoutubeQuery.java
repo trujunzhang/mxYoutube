b3b30721d57d3e80aff84d7322c0f1b9b92aee3e
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-27 09:05:01 +0800

    add tools:AppDaoGenerator and java2objc.

--- android/mxYoutubeApp/tools/java2objc/src/main/java/com/googlecode/java2objc/main/projects/AndroidYoutubeQuery.java
+++ android/mxYoutubeApp/tools/java2objc/src/main/java/com/googlecode/java2objc/main/projects/AndroidYoutubeQuery.java
@@ -2,24 +2,34 @@ package com.googlecode.java2objc.main.projects;
 
 public class AndroidYoutubeQuery {
 
+	// /Volumes/Home/Developing/SketchProjects/mxYoutube/android/mxYoutubeApp/ios/mxYoutubeApp/lib/android-youtube-query/Pod/Classes/YQVideoInfoTaskCallback.h
+	// /Volumes/Home/Developing/SketchProjects/mxYoutube/ios/mxYoutubeApp/lib/android-youtube-query/Pod/Classes/YQVideoInfoTaskCallback.h
+	// /Volumes/Home/Developing/SketchProjects/mxYoutube/callbackios/mxYoutubeApp/lib/android-youtube-query/Pod/Classes/YQVideoInfoTaskCallback.h
+	// /Volumes/Home/Developing/SketchProjects/mxYoutube/ios/mxYoutubeApp/lib/android-youtube-query/Pod/Classes/callback/YQVideoInfoTaskCallback.h
+
 	public static final String PROJECT_ROOT = "/Volumes/Home/Developing/SketchProjects/mxYoutube/";
 	public static final String PACKAGE_ROOT = "android/mxYoutubeApp/libraries/open/android-youtube-query/src/main/java/com/keyes/youtube/";
 
+	public static final String IOS_DIR = "ios/mxYoutubeApp/lib/android-youtube-query/Pod/Classes/";
 	public static final String OUTPUTDIR = "--outputdir=";
 	public static final String PREFIX_NAME = "--prefix=YQ";
 
 	public static String[] getArgs() {
 		String[] dirs = {// convert dir
-//		"beans",// 0
-				"callback",// 1
-//				"ui",// 2
-//				"utils",// 3
+		"beans",// 0
+		// "callback",// 1
+		// "ui",// 2
+		// "utils",// 3
+		};
+		String[] files = {// convert files
+		"",// default
+				"/VideoStream",// test
 		};
 
 		String[] args = new String[3];
 		int s = 0;
-		args[s++] = PROJECT_ROOT + PACKAGE_ROOT + dirs[0];
-		args[s++] = OUTPUTDIR + "ios/mxYoutubeApp/lib/android-youtube-query/Pod/Classes";
+		args[s++] = PROJECT_ROOT + PACKAGE_ROOT + dirs[0] + files[0];
+		args[s++] = OUTPUTDIR + PROJECT_ROOT + IOS_DIR + dirs[0] + files[0];
 		args[s++] = PREFIX_NAME;
 		return args;
 	}
