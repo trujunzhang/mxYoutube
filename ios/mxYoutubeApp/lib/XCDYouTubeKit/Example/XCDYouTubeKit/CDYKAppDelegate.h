f9220e7602b2a98c5370f0b4ebd3e33e232ecb67
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-30 08:31:39 +0800

    XCDYouTubeKit .

---
+++ ios/mxYoutubeApp/lib/XCDYouTubeKit/Example/XCDYouTubeKit/CDYKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDYKAppDelegate.h
+//  XCDYouTubeKit
+//
+//  Created by CocoaPods on 09/30/2014.
+//  Copyright (c) 2014 wanghaogithub720. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDYKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios/mxYoutubeApp/lib/XCDYouTubeKit/Example/XCDYouTubeKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XCDYouTubeKit
+//
+//  Created by wanghaogithub720 on 09/30/2014.
+//  Copyright (c) 2014 wanghaogithub720. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CDYKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CDYKAppDelegate class]));
+    }
+}
