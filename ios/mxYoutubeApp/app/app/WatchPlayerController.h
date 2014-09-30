936c0f63ea866e9798c74a2b7216f647b65df405
wanghaogithub720 <wanghaomspace@gmail.com>
2014-09-30 08:20:42 +0800

    add library "lib/XCDYouTubeKit".

---
+++ ios/mxYoutubeApp/app/app/WatchPlayerController.h
@@ -0,0 +1,13 @@
+//
+//  WatchPlayerController.h
+//  app
+//
+//  Created by djzhang on 9/30/14.
+//  Copyright (c) 2014 xinma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WatchPlayerController : UIViewController
+
+@end
