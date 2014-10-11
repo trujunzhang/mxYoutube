7275cb2527d350c2455d7c68cc105c65be066645
wanghaogithub720 <wanghaomspace@gmail.com>
2014-10-11 11:54:39 +0800

    fix changeBackground ima''ge.

--- ios/mxYoutubeApp/app/app/ViewController.m
+++ ios/mxYoutubeApp/app/app/ViewController.m
@@ -122,21 +122,9 @@
 
 
 - (void)changeBackground:(BOOL)portrait {
-
-   UIScreen * mainScreen = [UIScreen mainScreen];
-   CGFloat scale = ([mainScreen respondsToSelector:@selector(scale)] ? mainScreen.scale : 1.0f);
-   CGFloat pixelWidth = (CGRectGetWidth(mainScreen.bounds) * scale);
-   CGFloat pixelHeight = (CGRectGetHeight(mainScreen.bounds) * scale);
-
    NSString * name = [AppResolutionHelper resolutionNameByType:[UIDevice resolution] isPortrait:portrait];
-   if (portrait) {
-//      self.background.frame = CGRectMake(0, 0, 640, 1136/2);
-      self.background.frame = CGRectMake(0, 0, pixelWidth, pixelHeight / scale);
-   } else {
-      self.background.frame = CGRectMake(0, 0, pixelHeight, pixelWidth / scale);
-//      self.background.frame = CGRectMake(0, 0, 1024, 768);
-   }
-   self.background.frame = CGRectMake(0, 0, pixelWidth, pixelHeight / scale);// TODO djzhang
+   self.background.frame = [[UIScreen mainScreen] bounds];
+
 //   self.background.image = [UIImage imageNamed:@"Default-568h@2x.png"];
    self.background.image = [UIImage imageNamed:name];
 }
