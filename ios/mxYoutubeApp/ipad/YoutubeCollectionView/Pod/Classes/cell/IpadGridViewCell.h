2814644060924ab1d5ed0cbfc812df57ec24460a
wanghaogithub720 <wanghaomspace@gmail.com>
2014-10-20 10:34:41 +0800

    Ipad Gridview Cell.

--- ios/mxYoutubeApp/ipad/YoutubeCollectionView/Pod/Classes/cell/IpadGridViewCell.h
+++ ios/mxYoutubeApp/ipad/YoutubeCollectionView/Pod/Classes/cell/IpadGridViewCell.h
@@ -16,13 +16,10 @@
 
 @property(strong, nonatomic) IBOutlet UILabel * title;
 
-@property(strong, nonatomic) IBOutlet UILabel * rating;
-@property(strong, nonatomic) IBOutlet UILabel * viewCount;
-
 @property(strong, nonatomic) IBOutlet UIImageView * userHeader;
 @property(strong, nonatomic) IBOutlet UILabel * userName;
 
-@property(strong, nonatomic) IBOutlet UIView * infoView;
+@property (strong, nonatomic) IBOutlet UIView *infoView;
 
 
 - (void)bind:(GTLYouTubeVideo *)video placeholderImage:(UIImage *)image;
