2644d626c47a193a55e1fe0848019729e8d40025
wanghaogithub720 <wanghaomspace@gmail.com>
2014-10-20 18:22:05 +0800

    Ipad Gridview Cell.

---
+++ ios/mxYoutubeApp/module/VideoWatchDetailViewControlleriPad/Pod/Classes/videodetail/VideoDetailPanel.h
@@ -0,0 +1,13 @@
+//
+//  VideoDetailPanel.h
+//  YoutubeDetailRotateTabbarsApp
+//
+//  Created by djzhang on 10/9/14.
+//  Copyright (c) 2014 djzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoDetailPanel : UIView
+
+@end
